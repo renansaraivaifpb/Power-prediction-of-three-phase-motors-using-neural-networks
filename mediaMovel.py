33c561b3f4cbe2c1d5f615157089878493add5f2
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2022-12-13 11:46:16 -0300

    Update mediaMovel.py

--- mediaMovel.py
+++ mediaMovel.py
@@ -1,15 +1,16 @@
-def mediaMovel(windowSize, eixo, dados_):
-    # atualizando nosso dataFrame para ter apenas
-    # uma coluna 'Fechar' como resto todas as colunas
-    # são inúteis para nós no momento
+import pandas as pd 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+def calculeMediaMovel(windowSize, eixo, dados_):
     # usando .to_frame() para converter séries de pandas
     # no dataframe.
     dados = dados_[eixo].to_frame()
 
     # calculando média móvel simples
     # usando .rolling(window).mean() ,
-    # com tamanho da janela = 30
+    # com tamanho da janela = windowSize
     dados[eixo] = dados[eixo].rolling(windowSize).mean()
-    dados[[eixo]].plot(figsize=(16, 8))
+    dados.plot(figsize=(12, 5))
     sns.lineplot(data = dados) 
     plt.show()
