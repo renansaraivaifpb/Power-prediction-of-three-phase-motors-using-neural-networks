4a50b79822d40d088d32f4e8dc180d19e95aecb2
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-01-28 19:02:38 -0300

    Update jsonToDateframe.py
    Este código carrega dados de arquivos em um diretório específico, calcula médias móveis, variância, curtose e amplitude dos dados e os adiciona a um dataframe. Em seguida, o dataframe é mapeado para valores de classe de potência e salvo em um arquivo CSV. A função visualizarDados() é chamada no final, mas não é usada no código atual. Ele usa a biblioteca pandas para manipular os dados e as bibliotecas seaborn e matplotlib para visualizá-los. Ele também importa algumas funções personalizadas de arquivos externos para calcular média móvel, variância, curtose e amplitude.

--- jsonToDateframe.py
+++ jsonToDateframe.py
@@ -1,53 +1,72 @@
-# bibliotecas para conversão
 import pandas as pd
-import json
 
 # bibliotecas para visualizações
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# arquivo carregarDados.py
+from carregarDados import carregar_dados
+
 # arquivo mediaMovel.py
 from mediaMovel import calculeMediaMovel
 
+# arquivo variancia.py
+from variancia import calcularVariance
+
+# arquivo curtose.py
+from curtose import calcularCurtose
+
+# arquivo amplitude.py
+from amplitude import calcularAmplitude
+
+# arquivo salvarAmplitudeCsv.py
+# from salvarAmplitudeCsv import salvarAmplitudeCSV
 
 def visualizarDados(dados):
     ''' Função de plotagem '''
     sns.jointplot(data=dados)
     plt.show()
 
+conjunto = 'testing'
+pasta = conjunto+'/'
 
-def jsonToDataFrame(address_file):
-    ''' Função que retorna dado estruturado em DataFrame '''
-    with open(address_file) as jsonfile:
-        data = json.load(jsonfile)
-    # chave e valor
-    lista = data['payload']['values']
-    # transforma a lista em uma variavel do tipo dataframe com as seguintes,
-    # colunas: 'accX','accY','accZ'
-    df = pd.DataFrame (lista, columns = ['accX','accY','accZ'])
-    return df
+# Load data
+dados_list, classePotencia, nomeArquivo = carregar_dados(pasta)
 
+# numero total de listas
+# cada tabela representa um arquivo 
+# Calculate moving averages
+media_list = [calculeMediaMovel(25, dados_list[i]) for i in range(len(dados_list))]
 
-## comandos para selecionar o arquivo
-address = input("Digite local de arquivo: ")
-dados = jsonToDataFrame(address)
-# visualizarDados(dados)
-# POTENCIA 230W
-# Training/potencia-230W/A.3jjeujup.ingestion-5b88b9545f-7r6tj.json
-# POTENCIA 320W
-# Training/potencia-320W/B.3jjfahvd.ingestion-5b88b9545f-ltdhd.json
-# POTENCIA 620W
-# Training/potencia-620W/C.3jjfl9vo.ingestion-5b88b9545f-ltdhd.json
-# POTENCIA 1100W
-# Training/potencia-1100W/D.3jjg0o89.ingestion-5b88b9545f-ltdhd.json
+# Calculate variance
+variancia_list = [calcularVariance(25, dados_list[i]) for i in range(len(dados_list))]
+
+# Merge moving averages and variance
+df2_list = [pd.merge(media_list[i], variancia_list[i], right_index = True, left_index = True) for i in range(len(dados_list))]
 
 
+# Calculate kurtosis
+curtose_list = [calcularCurtose(25,dados_list[i]) for i in range(len(dados_list))]
 
 
-plotarMediaMovel = int(input("Digite o tamanho da janela: "))
-# separe os eixos com espaços
-verificarEixo = input("Digite o eixo(s) que deseja verificar: ")
-# quebrar a string que contem espaços
-# eixos = verificarEixo.split()
-calculeMediaMovel(plotarMediaMovel, verificarEixo, dados)
+# Merge df2 and kurtosis
+df3_list = [pd.merge(df2_list[i], curtose_list[i], right_index = True, left_index = True) for i in range(len(dados_list))]
 
+
+# Calculate amplitude
+amplitude_list = [calcularAmplitude(dados_list[i],10) for i in range(len(dados_list))]
+
+df1 = pd.DataFrame(amplitude_list)
+df2 = pd.DataFrame(classePotencia, columns=['Y'])
+
+# Mapeando valores da classe de potencia
+df2['Y'] = df2['Y'].map({'A': 0.22, 'B': 0.32, 'C': 0.62, 'D': 1.15, 'E': 0.27, 'F': 0.50, 'G': 0.85, 'H': 1.00})
+
+# Concatenando dataframes
+conjuntoDados = pd.concat([df1, df2], axis=1, ignore_index=True)
+
+# Renomeando colunas
+conjuntoDados.columns = list(df1.columns) + ['Y']
+
+# Salvando em arquivo CSV
+conjuntoDados.to_csv(conjunto+'.csv', index=False)
