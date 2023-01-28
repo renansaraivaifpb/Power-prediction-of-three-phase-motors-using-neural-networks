88fc54dfa318c7eb40687ca22de9815da275448a
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-01-28 19:21:14 -0300

    Create curtose.py
    A função acima calcula a curtose (medida de achatamento ou achatamento) para cada uma das colunas de aceleração (accX, accY, accZ) no conjunto de dados fornecido. A curtose é calculada usando a função rolling do pandas para calcular a curtose para janelas de tamanho "windowSize" nos dados. O resultado é retornado como um DataFrame com colunas renomeadas como "curtose_accX", "curtose_accY" e "curtose_accZ".

---
+++ curtose.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import scipy.stats
+def calcularCurtose(windowSize, dados_):
+    s = pd.DataFrame(dados_).rolling(windowSize).kurt()
+    s = s.rename(columns={'mediaMovel_accX': 'curtose_accX', 'mediaMovel_accY': 'curtose_accY', 'mediaMovel_accZ': 'curtose_accZ'})
+    return s
