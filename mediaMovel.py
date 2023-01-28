3344e29929efec0aa2e838af6d333b1eb6aa43cf
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-01-28 19:06:43 -0300

    Update mediaMovel.py
    Essa função tem como objetivo calcular a média móvel dos dados. Ela usa o argumento windowSize para especificar o tamanho da janela de tempo que será usada para calcular a média móvel. Ele também usa o argumento dados_, que é um dataframe do pandas, e adiciona três novas colunas para os dados de aceleração X, Y e Z, chamadas 'mediaMovel_accX', 'mediaMovel_accY' e 'mediaMovel_accZ', respectivamente. Estas colunas contêm os valores de média móvel dos dados de aceleração. A função retorna o dataframe modificado.

--- mediaMovel.py
+++ mediaMovel.py
@@ -3,19 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def calculeMediaMovel(windowSize, dados_):
-    dados1 = dados_['accX'].to_frame()
-    dados1['accX'] = dados1['accX'].rolling(windowSize).mean()
-
-    dados2 = dados_['accY'].to_frame()
-    dados2['accY'] = dados2['accY'].rolling(windowSize).mean()
-
-    dados3 = dados_['accZ'].to_frame()
-    dados3['accZ'] = dados3['accZ'].rolling(windowSize).mean()
-
-    df1 = pd.merge(dados1['accX'], dados2['accY'], right_index = True,
-               left_index = True)
-
-    df2 = pd.merge(df1, dados3['accZ'], right_index = True,
-               left_index = True)
-    df2 = df2.rename(columns={'accX': 'mediaMovel_accX', 'accY': 'mediaMovel_accY', 'accZ': 'mediaMovel_accZ'})
-    return df2
+    dados_['mediaMovel_accX'] = dados_['accX'].rolling(windowSize).mean()
+    dados_['mediaMovel_accY'] = dados_['accY'].rolling(windowSize).mean()
+    dados_['mediaMovel_accZ'] = dados_['accZ'].rolling(windowSize).mean()
+    return dados_
