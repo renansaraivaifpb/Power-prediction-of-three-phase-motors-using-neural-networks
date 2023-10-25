5771f676726a786f9f2109b3e3d17a5b79c1bbdf
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-10-25 10:20:22 -0300

    Update and rename variancia.py to variance.py

--- variance.py
+++ variance.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import scipy.stats
-def calcularVariance(windowSize, dados_):
+def calculateVariance(windowSize, dados_):
     dados_['variancia_accX'] = dados_['mediaMovel_accX'].rolling(windowSize).var()
     dados_['variancia_accY'] = dados_['mediaMovel_accY'].rolling(windowSize).var()
     dados_['variancia_accZ'] = dados_['mediaMovel_accZ'].rolling(windowSize).var()
