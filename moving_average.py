37fb73e08986592ecdae5c753c3d6bc80e466660
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-10-25 10:23:57 -0300

    Update and rename mediaMovel.py to moving_average.py

--- moving_average.py
+++ moving_average.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
-def calculeMediaMovel(windowSize, dados_):
+def calculateMoving_average(windowSize, dados_):
     dados_['mediaMovel_accX'] = dados_['accX'].rolling(windowSize).mean()
     dados_['mediaMovel_accY'] = dados_['accY'].rolling(windowSize).mean()
     dados_['mediaMovel_accZ'] = dados_['accZ'].rolling(windowSize).mean()
