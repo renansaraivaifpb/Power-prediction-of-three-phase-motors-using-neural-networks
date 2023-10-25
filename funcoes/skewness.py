63f7ca558068d39ede6c59351ca3c3731cc57d26
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-10-25 10:30:47 -0300

    Update skewness.py

--- funcoes/skewness.py
+++ funcoes/skewness.py
@@ -1,8 +1,8 @@
 import pandas as pd 
 from scipy.stats import skew
-# Skewness (assimetria) de uma distribuição de dados
+# Skewness (asymmetry) of a data distribution
 def calcularSkewness(dados_):
-    # calcular a Skewness dos dados de aceleração
+    # calculate the skewness of acceleration data
     dados_['accX Skewness'] = skew(dados_['accX'])
     dados_['accY Skewness'] = skew(dados_['accY'])
     dados_['accZ Skewness'] = skew(dados_['accZ'])
