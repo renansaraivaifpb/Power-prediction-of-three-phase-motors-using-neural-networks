d162542aefbb468e35fe048110daee4fefeaeb2a
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-10-25 10:31:35 -0300

    Update curtose.py

--- funcoes/curtose.py
+++ funcoes/curtose.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 def calcularCurtose(windowSize, dados_):
+    ''' Calculate Kurtosis '''
     dados_['accX Kurtosis'] = dados_['accX'].rolling(windowSize).kurt()
     dados_['accY Kurtosis'] = dados_['accY'].rolling(windowSize).kurt()
     dados_['accZ Kurtosis'] = dados_['accZ'].rolling(windowSize).kurt()
