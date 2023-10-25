e4792f7046e3181d0bdccaf9ce1dc99decc5bc42
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2023-10-25 10:22:52 -0300

    Update amplitude.py

--- amplitude.py
+++ amplitude.py
@@ -1,9 +1,9 @@
 import numpy as np
-def calcularAmplitude(dados_, x):
+def calculateAmplitude(dados_, x):
     '''
-     o valor de x é o número de pontos mais altos ou mais baixos que você deseja considerar. 
-     Por exemplo, se você quiser calcular a amplitude dos 10 pontos mais altos e mais baixos da 
-     lista, basta definir x como 10 
+     the value of x is the number of highest or lowest points you want to consider.
+      For example, if you want to calculate the amplitude of the 10 highest and lowest points in the
+      list, just set x to 10 
     '''
     df_sorted = dados_.sort_values(by=['accX', 'accY', 'accZ'], ascending=False)
     return np.mean(df_sorted[x:], axis=0) - np.mean(df_sorted[:x], axis=0)
